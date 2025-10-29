a8268e05c801df3f8bca0abe6e44a4d7caf97cf8
mari22q2 <mfhvelplund@gmail.com>
2025-10-29 16:09:37 +0100

    Revert "init popup test"
    This reverts commit 3544004857d4ce6c42a9b3e56ec314a5199cf5bb.

--- assets/js/openPopup.js
+++ assets/js/openPopup.js
@@ -1,13 +1,3 @@
-// Immediately define a queue for early initPopups calls
-window._popupQueue = window._popupQueue || [];
-
-// Define initPopups stub immediately so it's available even if script hasn't finished loading
-if (!window.initPopups) {
-  window.initPopups = function(config) {
-    window._popupQueue.push(config);
-  };
-}
-
 (function() {
   // Core popup function
   function openPopup(formUrl, brandColor = '#0066FF', width = 'auto', height = 'auto') {
@@ -304,15 +294,7 @@ if (!window.initPopups) {
     autoInit();
   }
 
-  // Process queued configs
-  if (window._popupQueue && window._popupQueue.length > 0) {
-    window._popupQueue.forEach(config => {
-      initPopups(config);
-    });
-    window._popupQueue = [];
-  }
-
-  // Expose to global scope (replaces the stub with the real function)
+  // Expose to global scope
   window.openPopup = openPopup;
   window.initPopups = initPopups;
 })();
